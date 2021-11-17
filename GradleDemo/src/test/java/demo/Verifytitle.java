a1324d9af5c8d58009222f6243a4a2e81e3704b6
saajid shaik <saajidshaik19@gmail.com>
2021-11-17 11:36:53 +0000

    lab projects upto date

---
+++ GradleDemo/src/test/java/demo/Verifytitle.java
@@ -0,0 +1,37 @@
+package demo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Verifytitle {
+	ChromeDriver driver;
+
+	@BeforeTest
+	public void  launchApplication() {
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://linkedin.com/");
+	}
+	
+	@Test
+	public void titleVerification() {
+		String expectedTitle ="LinkedIn: Log In or Sign Up";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+		
+	}
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+	}
+	}
+	
+	
+	
+
+
--- java1/src/exptionhandling/Calc.java
+++
@@ -1,5 +0,0 @@
-package exptionhandling;
-
-public class Calc {
-
-}
